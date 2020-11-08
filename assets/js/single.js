ed8a4a42fedb33220adfdd04ffefbb2e9b3340e0
Xavier <X64515@yahoo.com>
2020-11-07 19:44:44 -0600

    117

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,45 @@
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+getRepoName=()=>{
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    console.log(repoName);
+}
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if (repoName){
+      
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  }
+  else {
+    document.location.replace("./index.html");
+  }
+}
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
 var getRepoIssues = function(repo) {
+    if (response.ok) {
+        response.json().then(function(data) {
+          displayIssues(data);
+      
+          // check if api has paginated issues
+          if (response.headers.get("Link")) {
+            displayWarning(repo)
+          }
+        });
+      }
     console.log(repo);
   };
   
@@ -8,18 +49,23 @@ var getRepoIssues = function(repo) {
     issueContainerEl.textContent = "This repo has no open issues!";
     return;
   }
-  fetch(apiUrl).then(function(response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function(data) {
-        // pass response data to dom function
-        displayIssues(data);
-      });
-    }
-    else {
-      alert("There was a problem with your request!");
-    }
-  });
+// make a get request to url
+fetch(apiUrl).then(function(response) {
+  // request was successful
+  if (response.ok) {
+    response.json().then(function(data) {
+      displayIssues(data);
+
+      // check if api has paginated issues
+      if (response.headers.get("Link")) {
+        displayWarning(repo);
+      }
+    });
+  } else {
+    // if not successful, redirect to homepage
+    document.location.replace("./index.html");
+  }
+});
   var displayIssues = function(issues) {
 
     fetch(apiUrl).then(function(response) {
@@ -64,3 +110,4 @@ issueEl.appendChild(typeEl);
       });
 
 };
+getRepoName()
